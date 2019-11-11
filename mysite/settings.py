4e2381a899b45467bcb4e96318c523ed28798b4b
josegarban <josegarban@gmail.com>
2019-11-11 00:14:12 -0400

    Added todo templates and static.

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,14 +72,19 @@ SESSION_SECURITY_EXPIRE_AFTER = 12
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["mysite/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
+                # Your stuff: custom template context processors go here
             ],
         },
     },
