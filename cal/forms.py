4946f697acc6b4581256294180b497dd0e731d5c
josegarban <josegarban@gmail.com>
2019-11-13 18:36:35 -0400

    Events added at the correct time accounting for timezone differences.

--- cal/forms.py
+++ cal/forms.py
@@ -1,18 +1,21 @@
-from django.forms import ModelForm, DateInput
+from django import forms
 from cal.models import Event
 
-class EventForm(ModelForm):
-  class Meta:
-    model = Event
-    # datetime-local is a HTML5 input type, format to make date time show on fields
-    widgets = {
-      'start_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%d %H:%M'),
-      'end_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%d %H:%M'),
-    }
-    fields = '__all__'
+class EventForm(forms.ModelForm):
 
-  def __init__(self, *args, **kwargs):
-    super(EventForm, self).__init__(*args, **kwargs)
-    # input_formats parses HTML5 datetime-local input to datetime field
-    self.fields['start_time'].input_formats = ('%Y-%m-%d %H:%M',)
-    self.fields['end_time'].input_formats = ('%Y-%m-%d %H:%M',)
+    class Meta:
+        model = Event
+        # datetime-local is a HTML5 input type, format to make date time show on fields
+        widgets = {
+          # 'start_time': forms.DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%d %H:%M'),
+          # 'end_time': forms.DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%d %H:%M'),
+          'start_time': forms.DateInput(format='%Y-%m-%d %H:%M'),
+          'end_time': forms.DateInput(format='%Y-%m-%d %H:%M'),
+        }
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+      super(EventForm, self).__init__(*args, **kwargs)
+      # input_formats parses HTML5 datetime-local input to datetime field
+      self.fields['start_time'].input_formats = ('%Y-%m-%d %H:%M',)
+      self.fields['end_time'].input_formats = ('%Y-%m-%d %H:%M',)
